cd2e0244a592669eb76f487bca0846147b5f0f53
Braun Shedd <braun@braunshedd.com>
2015-08-16 03:19:06 -0700

    working navigation animations

---
+++ www/js/navigation.js
@@ -0,0 +1,74 @@
+var tic = true;
+$("#arrows").transit({rotateY: "40deg"}, 0);
+$("#next").transit({rotateY: "-40deg"}, 0);
+$("#street").transit({rotateY: "40deg"}, 0);
+$("#city").transit({rotateY: "-40deg"}, 0);
+$("#blue").transit({rotateY: "40deg"}, 0);
+$("#card1").transit({rotateY: "90deg"}, 0);
+$("#card2").transit({rotateY: "90deg"}, 0);
+$("#card3").transit({rotateY: "90deg"}, 0);
+
+
+$(document).ready(function()
+{
+	setTimeout(function()
+	{
+		$("#next").transit({opacity: 1, rotateY: 0}, 1000);
+		setTimeout(function()
+		{
+			$("#street").transit({opacity: 1, rotateY: 0}, 1000);
+			setTimeout(function()
+			{
+				$("#city").transit({opacity: 1, rotateY: 0}, 1000);
+				setTimeout(function()
+				{
+					$("#blue").transit({opacity: "1", rotateY: 0}, 500);
+					setTimeout(function()
+					{
+						$("#card1").transit({rotateY: "0"}, 1000);
+						setTimeout(function()
+						{
+							$("#card2").transit({rotateY: "0"}, 1000);
+							setTimeout(function()
+							{
+								$("#card3").transit({rotateY: "0"}, 1000);
+							}, 1000);
+						}, 1000);
+					}, 1000);
+				}, 1000);
+			}, 1000);
+		}, 1000);
+	}, 1000);
+
+
+	setTimeout(function()
+	{
+		$("#arrows").transit({opacity: 1, rotateY: 0}, 1000);
+		setInterval(function()
+		{
+			if (tic) {
+				var text = $("#arrows").text();
+				var ret = text.substring(2, text.length);
+				$("#arrows").transit({opacity: 0.8}, 200);
+				setTimeout(function()
+				{
+					$("#arrows").text(ret);
+					$("#arrows").transit({opacity: 1}, 200);
+				}, 200);
+				tic = false;
+			}
+			else //toc
+			{
+				var text = $("#arrows").text();
+				var ret = text + " >";
+				$("#arrows").transit({opacity: 0.8}, 200);
+				setTimeout(function()
+				{
+					$("#arrows").text(ret);
+					$("#arrows").transit({opacity: 1}, 200);
+				}, 200);
+				tic = true;
+			}
+		}, 1000);
+	}, 8500);
+});
